2c2550d03dea40dca4fcc3a72b11c5c4562462ba
burakbeskazalioglu <burakb7@icloud.com>
2023-06-26 20:51:09 -0500

    new

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hell");
+        }
+    }
 }
