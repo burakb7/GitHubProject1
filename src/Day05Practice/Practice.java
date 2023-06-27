f28c05a185e4486e8a2edf404dd4e9ec36df69e6
burakbeskazalioglu <burakb7@icloud.com>
2023-06-27 13:32:07 -0500

    created practice

--- src/Day05Practice/Practice.java
+++ src/Day05Practice/Practice.java
@@ -3,5 +3,7 @@ package Day05Practice;
 public class Practice {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("say");
+        System.out.println("burak");
     }
 }
