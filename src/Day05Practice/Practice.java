ab2feee8c4141b6017df3a4964c03755757364eb
burakbeskazalioglu <burakb7@icloud.com>
2023-06-27 13:38:32 -0500

    branch work

--- src/Day05Practice/Practice.java
+++ src/Day05Practice/Practice.java
@@ -4,5 +4,6 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("beb");
+        System.out.println("try");
     }
 }
